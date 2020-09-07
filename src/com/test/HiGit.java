577198a144bcdb7a2de9a9d8e144864d7bba407e
PrettyBoyi <1369690254@qq.com>
2020-09-07 14:45:35 +0800

    lxl-1

--- src/com/test/HiGit.java
+++ src/com/test/HiGit.java
@@ -8,8 +8,6 @@ public class HiGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("test");
-
-
-
+        System.out.println("啥也不是！！！！！！");
     }
 }
