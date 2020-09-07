fcb38b128a6ae56e7f45159b2d7cd7eb894da4e8
PrettyBoyi <1369690254@qq.com>
2020-09-07 11:59:53 +0800

    test

---
+++ src/com/test/HiGit.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author lixiaolong
+ * @creat 2020-09-07 11:44
+ */
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
